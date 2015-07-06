8e53b2263c6f1aaec7cf05969888d959a2a67ecd
Kerry Rosenberg <rosenberg.kerry@gmail.com>
2015-07-06 13:48:39 -0500

    Working on incrementing, decrementing and questions.

---
+++ BasicMath/global.js
@@ -0,0 +1,10 @@
+var numOne = Number(prompt("Please enter a number"));
+var numTwo = Number(prompt("Please enter a second number"));
+var numThree = Number(prompt("Please enter a third number"));
+
+alert(numOne + numTwo + numThree);
+alert(numOne - numTwo - numThree);
+alert(numOne * numTwo * numThree);
+alert(numOne / numTwo);
+alert(numOne++);
+alert(numTwo--);
