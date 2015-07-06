36e79b13fe0d33e1ca12c899d6730cd88e4f0f31
Kerry Rosenberg <rosenberg.kerry@gmail.com>
2015-07-06 15:37:29 -0500

    Questions and exercises through 6D

--- IfStatements/global.js
+++ IfStatements/global.js
@@ -17,14 +17,61 @@ console.log( "5 > 12 is " + (5 > 12)); //false
 console.log( "5 >= 12 is " + (5 >= 12)); //false
 
 
-var password = "monkeybrains"
-userGuess = prompt("Please enter your password:");
+// var password = "monkeybrains";
+// userGuess = prompt("Please enter your password:");
+//
+// if (userGuess === password)
+// {
+//   alert("Good job!")
+// }
+// else
+// {
+//   alert("Incorrect password.")
+// }
 
-if (userGuess === password)
+// var secretNumber = 23;
+// userNumber = prompt("Guess the secret number:");
+// // userNumber = Number(userNumber);
+//
+// if (userNumber == secretNumber)
+// // userNumber === secretNumber
+// {
+//   alert("Congratulations! You guessed correctly!")
+// }
+// else
+// {
+//   alert("Guess does not match secret number.")
+// }
+
+userAge = prompt("How old are you?")
+userAge = Number(userAge)
+
+if (userAge >= 25)
 {
-  alert("Good job!")
+  alert("You can rent a car.")
 }
-else
+
+if (userAge >= 21)
 {
-  alert("Incorrect password.")
+  alert("Please don't drink and drive.")
+}
+
+if (userAge >= 16)
+{
+  alert("You are eligible for a driver's license.")
+}
+
+if (0 <= userAge && userAge <= 15)
+{
+  alert("You can't drive yet, but you'll be able to in a few years.")
+}
+
+if (userAge < 0)
+{
+  alert("You haven't been born yet.")
+}
+
+if (isNaN(userAge))
+{
+  alert("Sorry that is not a number.")
 }
