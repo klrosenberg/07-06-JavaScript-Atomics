bf706caf8f3d6aa355d763bc813d9211f376d34a
Kerry Rosenberg <rosenberg.kerry@gmail.com>
2015-07-06 13:23:48 -0500

    Prompt input and questions answered.

---
+++ Input/global.js
@@ -0,0 +1,19 @@
+//Store response to promt in variable.
+var name = prompt("What is your name?");
+var age = prompt("How old are you?");
+var hometown = prompt("Where is your hometown?");
+var favNumber = prompt("What is your favorite number?");
+
+//Print responses to the console.
+console.log("Your name is " + name);
+console.log("You are " + age + " years old.");
+console.log("You are from " + hometown);
+console.log("Your favorite number is " + favNumber);
+
+typeof age;
+
+//Print the responses in alerts.
+alert("Your name is " + name);
+alert("You are " + age + " years old.");
+alert("You are from " + hometown);
+alert("Your favorite number is " + favNumber);
---
+++ Variables/global.js
@@ -0,0 +1,7 @@
+var name = "Kerry";
+var age = 25
+var hairColor = "brown";
+
+alert("My name is " + name);
+alert("My age is " + age);
+alert("My hair color is " + hairColor);
