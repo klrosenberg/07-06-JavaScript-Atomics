2d8cbb76aa624c279c19fdb2d606d39635df14e7
Kerry Rosenberg <rosenberg.kerry@gmail.com>
2015-07-06 11:38:50 -0500

    Alerts and console.log

---
+++ Output/global.js
@@ -0,0 +1,13 @@
+alert("Hello! My name is Kerry.");
+
+console.log("My favorite place to eat breakfast is Dixie Quicks.");
+
+console.log("I don't have any cats.");
+
+console.log("La Buvette");
+
+console.log("I take about 2 naps a week.");
+
+console.log("I'd like to take a nap everyday.");
+
+alert("Goodbye!");
