8594d9088667d521b227677d5ea64be8e9abeff1
Kerry Rosenberg <rosenberg.kerry@gmail.com>
2015-07-06 22:38:15 -0500

    Finished 6F triangle stretches.

---
+++ IfStatements/6F-Isosceles/global.js
@@ -0,0 +1,21 @@
+var sideOne = prompt("Please enter the first side length:")
+sideOne = Number(sideOne)
+
+var sideTwo = prompt("Please enter the second side length:")
+sideTwo = Number(sideTwo)
+
+var sideThree = prompt("Please enter the third side length:")
+sideThree = Number(sideThree)
+
+if (sideOne == sideTwo || sideTwo == sideThree || sideOne == sideThree)
+{
+  alert("This is a valid, isosceles triangle.")
+}
+else if (sideOne + sideTwo > sideThree && sideTwo + sideThree > sideOne && sideOne + sideThree > sideTwo)
+{
+  alert("This is a valid triangle.")
+}
+else
+{
+  alert("This is not a valid triangle.")
+}
