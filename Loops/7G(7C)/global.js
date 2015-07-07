593b163570170077aef161a386e4d5736caf7925
Kerry Rosenberg <rosenberg.kerry@gmail.com>
2015-07-07 09:50:31 -0500

    For loop completed for 99 bottles of beer on the wall.

---
+++ Loops/7G(7C)/global.js
@@ -0,0 +1,8 @@
+var text = ""
+var count = 99
+
+
+for (count; count > 1; count--) {
+    console.log(text += count + " bottles of beer on the wall, " + count + "bottles of beer! Take one down, pass it around...");
+}
+console.log("1 bottle of beer on the wall, 1 bottle of beer! Take one down, pass it around, no more bottles of beer on the wall.")
