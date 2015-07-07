b3f8b5747ce50ca79eaf567b60c7f1789a59e875
Kerry Rosenberg <rosenberg.kerry@gmail.com>
2015-07-06 23:49:42 -0500

    Beginning to refactor while loops to for loops.

---
+++ Loops/7F(7B)/global.js
@@ -0,0 +1,24 @@
+var startNum = prompt("With what number would you like to start the count?")
+startNum = Number(startNum)
+var endNum = prompt("With what number would you like to end the count?")
+endNum = Number(endNum)
+
+// If startNum is smaller, count up to the endNum.
+if (startNum < endNum)
+{
+  for (startNum; startNum <= endNum; startNum++) {
+      console.log(startNum);
+  }
+}
+// If startNum is larger, count down to the endNum.
+else if (endNum < startNum)
+{
+  while (endNum; endNum <= startNum; startNum--) {
+      console.log(startNum);
+  }
+}
+// Else, numbers are the same and count cannot be incremented by 0.
+else
+{
+  console.log("Cannot count by an increment of 0.")
+}
