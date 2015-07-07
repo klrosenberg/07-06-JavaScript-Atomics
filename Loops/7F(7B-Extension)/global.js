08c86bda545504a81007b22eb569153a77e8a689
Kerry Rosenberg <rosenberg.kerry@gmail.com>
2015-07-07 09:46:00 -0500

    Finished 7F for loops plus extension. Beginning 7G for loops.

--- Loops/7F(7B-Extension)/global.js
+++ Loops/7F(7B-Extension)/global.js
@@ -2,18 +2,20 @@ var startNum = prompt("With what number would you like to start the count?")
 startNum = Number(startNum)
 var endNum = prompt("With what number would you like to end the count?")
 endNum = Number(endNum)
+var counter = prompt("What number would you like to count by?")
+counter = Number(counter)
 
 // If startNum is smaller, count up to the endNum.
 if (startNum < endNum)
 {
-  for (startNum; startNum <= endNum; startNum++) {
+  for (startNum; startNum <= endNum; startNum+= counter) {
       console.log(startNum);
   }
 }
 // If startNum is larger, count down to the endNum.
 else if (endNum < startNum)
 {
-  for (startNum; endNum <= startNum; startNum--) {
+  for (startNum; endNum <= startNum; startNum-= counter) {
       console.log(startNum);
   }
 }
